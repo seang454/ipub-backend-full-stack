d51b46b61a74f4015d26f7d4bea5128cbb4816fe
ButSeavThong <rope61963@gmail.com>
2025-08-25 07:32:21 +0700

    admin processing

---
+++ src/main/java/com/istad/docuhub/contoller/AdminController.java
@@ -0,0 +1,11 @@
+package com.istad.docuhub.contoller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+
+}
--- src/main/java/com/istad/docuhub/domain/User.java
+++ src/main/java/com/istad/docuhub/domain/User.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(nullable = false, unique = true)
