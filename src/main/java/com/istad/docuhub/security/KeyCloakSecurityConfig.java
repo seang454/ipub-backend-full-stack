ca91259466e0e8c42c6702e57ad4c569dd8fd058
ruppstudent <pengseangsim210@gmail.com>
2025-09-07 23:07:56 +0700

    new ci

--- src/main/java/com/istad/docuhub/security/KeyCloakSecurityConfig.java
+++ src/main/java/com/istad/docuhub/security/KeyCloakSecurityConfig.java
@@ -109,10 +109,18 @@ public class KeyCloakSecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
+                      // .loginPage("/api/v1/auth/login")  do not // user Spring automatically redirects to Keycloak login page.
                         .userInfoEndpoint(userInfo -> userInfo.oidcUserService(new OidcUserService()))
-                        .successHandler((request, response, authentication) -> {
-                            response.sendRedirect("http://localhost:3000"); // <-- your frontend URL
-                        })
+                                .successHandler((request, response, authentication) -> {
+                                HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+                                SavedRequest savedRequest = requestCache.getRequest(request, response);
+                                    if (savedRequest != null) {
+                                        // Redirect to the URL user originally wanted
+//                                        String targetUrl = savedRequest.getRedirectUrl();
+//                                        response.sendRedirect(targetUrl);
+                                        response.sendRedirect("http://localhost:3000");
+                                    }
+                                })
                 )
                 // JSON response for unauthenticated API requests
                 .exceptionHandling(exception -> exception
