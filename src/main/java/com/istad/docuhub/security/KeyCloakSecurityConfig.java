19b095c81ff0e8c98eee9dcb4b329a6ed39e926e
ruppstudent <pengseangsim210@gmail.com>
2025-09-10 08:53:24 +0700

    update token with cookie

--- src/main/java/com/istad/docuhub/security/KeyCloakSecurityConfig.java
+++ src/main/java/com/istad/docuhub/security/KeyCloakSecurityConfig.java
@@ -50,7 +50,7 @@ public class KeyCloakSecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","https://new-add-to-card-hw-v1ia.vercel.app/")
+                        .allowedOrigins("http://localhost:3000","https://new-add-to-card-hw-v1ia.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
